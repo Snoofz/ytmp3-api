3203c817eb94ccd4d365501fc2b6988da5f08dd1
Snoofz <165219710+Snoofz@users.noreply.github.com>
2024-07-22 02:35:16 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Download yt-dlp
+wget https://github.com/yt-dlp/yt-dlp/releases/download/2024.07.16/yt-dlp_linux -O yt-dlp
+
+# Make the downloaded file executable
+chmod +x yt-dlp
+
+# Run yt-dlp
+./yt-dlp
